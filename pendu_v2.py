2a31c396eee9eea189ae2114d4fa1702d2f9f7aa
ysnztrk021 <yamoz1030@gmail.com>
2022-02-10 12:46:57 +0100

    ok

---
+++ pendu_v2.py
@@ -0,0 +1,67 @@
+import random
+import os
+import time
+from penu import hang
+def get_word():
+    with open('mots.txt', 'r') as file:
+        rando = file.read()
+        rand = list(map(str, rando.split()))
+        word = random.choice(rand)
+        return word
+
+word = get_word()
+
+def dash():
+    dashs=[]
+    for i in range(len(word)):
+        dashs.append('_')
+    return dashs
+
+dash = dash()
+
+def play():
+    chance = 8
+    print("\nBIENVENUE AU JEU 'BONHOMME PENDU'\n\n")
+    guess = list(dash)
+    wrong=[]
+    hang.reverse()
+
+    while chance > 0:
+        print(hang[chance-1])
+        print(''.join(guess),'\n')
+        guessed = input("Devinez la lettre/ le mot ! \n")
+        
+        if guessed.upper() in wrong:
+            print('Vous avez déjà introduit cette lettre')
+            time.sleep(1)
+            os.system('cls')
+
+        else:
+                
+            if guessed.upper() in word:
+                for i in range(len(word)):
+                    if word[i]==guessed.upper():
+                        guess[i]=guessed.upper()
+                print(''.join(guess))
+                time.sleep(1)
+                os.system('cls')
+                
+            elif guessed not in word :
+                wrong.append(guessed)
+                print('Vous avez entrez la mauvaise lettre/mot')
+                chance-=1
+                time.sleep(1)
+                os.system('cls')
+                
+            if ''.join(guess)==word:
+                print("Félicitation, vous avez trouvé le mot !")
+                break
+            
+        if len(wrong) > 0 and chance > 0:
+            print('Voici les mauvaises lettres que vous avez déjà introduit \n','[',','.join(wrong),']','\n','Et il vous reste ',chance,' chance(s)',sep='')
+                
+    if chance ==0:
+        print("Vous avez perdu !")
+        print("Le mot à trouver était ",word)
+        
+play()
